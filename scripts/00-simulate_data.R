040e1095f2e9cc9df29becf69f954a1816adf90f
zhouyang <cocoyang.zhou@mail.utoronto.ca>
2024-01-23 00:42:12 -0500

    skeleton updated

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,28 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+# Employment income (with cpp/ei deducted)
+# other income (benefits, allowance, credits)
+# Total Income
+
+# Average Monthly Rent
+# Food
+# Childcare
+# Transportation
+# Total Expense
+
+
---
+++ scripts/01-download_data.R
@@ -0,0 +1,24 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from "open data toronto, cost of living"
+# Author: Yang Zhou
+# Date: Jan 22. 2023
+# Contact: cocoyang.zhou@mail.utoronto.ca
+# License: MIT
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+
+#### Download data ####
+data <- search_packages("cost-of-living-in-toronto-for-low-income-households")
+res <- list_package_resources(data)
+
+
+#### Save data ####
+for(i in 1:nrow(res)){
+  write.csv(get_resource(res[i,]), file = paste("inputs/data/", res[i,]$name, ".csv"))
+}
+
+
+         

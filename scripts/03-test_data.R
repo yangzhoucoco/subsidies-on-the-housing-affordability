09a27efc57806e9ab89ba6333afd01f4460c20c3
zhouyang <cocoyang.zhou@mail.utoronto.ca>
2024-01-24 22:40:08 -0500

    Update 03-test_data.R
    finished simple testing of cleaned data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,40 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simple checks/testing on the data
+# Author: Yang Zhou
+# Date: 24 Jan 2024 
+# Contact: cocoyang.zhou@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
+# our tests will check if the minimum of rent or income is a positive number
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+
+checks <- function(path){
+  data <- read_csv( 
+    file = path,
+    show_col_types = FALSE
+  )
+  if(data$total_income_per_month |> min() < 0){
+    return(FALSE)
+  }
+  if(data$rent_per_month |> min() < 0){
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+if(!checks("outputs/data/with subsidies.csv")){
+  print("FAILED")
+}
+if(!checks("outputs/data/without subsidies, average rent.csv")){
+  print("FAILED")
+}
+if(!checks("outputs/data/without subsidies, market rent.csv")){
+  print("FAILED")
+}
+
+print("PASSED")
+
